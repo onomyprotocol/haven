0fe49e8f60ddab54d526cdd919b33cd29f253d01
Charles Dusek <dusek@onomy.io>
2023-04-24 00:30:38 -0500

    Add UID

---
+++ x/haven/keeper/uid.go
@@ -0,0 +1,35 @@
+package keeper
+
+import (
+	"encoding/binary"
+
+	"github.com/cosmos/cosmos-sdk/store/prefix"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/pendulum-labs/market/x/market/types"
+)
+
+// Get UID Count
+func (k Keeper) GetUidCount(ctx sdk.Context) uint64 {
+	store := prefix.NewStore(ctx.KVStore(k.storeKey), []byte{})
+	byteKey := types.KeyPrefix(types.UidKey)
+	bz := store.Get(byteKey)
+
+	// Count doesn't exist: no element
+	if bz == nil {
+		return 0
+	}
+
+	// Parse bytes
+	return binary.BigEndian.Uint64(bz)
+}
+
+// Increment UID Count
+func (k Keeper) IncUidCount(ctx sdk.Context) {
+	store := prefix.NewStore(ctx.KVStore(k.storeKey), []byte{})
+	byteKey := types.KeyPrefix(types.UidKey)
+	bz := store.Get(byteKey)
+	uid := binary.BigEndian.Uint64(bz)
+	uid++
+	binary.BigEndian.PutUint64(bz, uid)
+	store.Set(byteKey, bz)
+}
