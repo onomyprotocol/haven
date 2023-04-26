a190ef7a69bf6527a214d8efbd0105ad6d22a55b
Charles Dusek <dusek@onomy.io>
2023-04-25 22:45:43 -0500

    Add create-post logic

--- x/haven/keeper/msg_server_create_post.go
+++ x/haven/keeper/msg_server_create_post.go
@@ -3,15 +3,35 @@ package keeper
 import (
 	"context"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"haven/x/haven/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 func (k msgServer) CreatePost(goCtx context.Context, msg *types.MsgCreatePost) (*types.MsgCreatePostResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	// Test if haven exists
+	havenUid := k.GetHavenUid(ctx, msg.Haven)
+	if havenUid == 0 {
+		return nil, sdkerrors.Wrapf(types.ErrHavenDoesNotExist, "%s", msg.Haven)
+	}
+
+	// Create the uid
+	count := k.GetUidCount(ctx)
+
+	post := types.Post{
+		Uid:   count,
+		Title: msg.Title,
+		Body:  msg.Body,
+		Haven: havenUid,
+		Tips:  sdk.NewCoin("kudos", sdk.NewInt(0)),
+	}
+
+	k.SetPost(ctx, post)
+
+	k.IncUidCount(ctx)
 
 	return &types.MsgCreatePostResponse{}, nil
 }
