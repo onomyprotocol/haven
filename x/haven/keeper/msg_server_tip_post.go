0fdf2c527d30505a1e7f195d5b0ca1c21ba280ec
Charles Dusek <dusek@onomy.io>
2023-04-29 23:52:33 -0500

    Saving progress

--- x/haven/keeper/msg_server_tip_post.go
+++ x/haven/keeper/msg_server_tip_post.go
@@ -2,16 +2,39 @@ package keeper
 
 import (
 	"context"
+	"strconv"
+
+	"haven/x/haven/types"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"haven/x/haven/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 func (k msgServer) TipPost(goCtx context.Context, msg *types.MsgTipPost) (*types.MsgTipPostResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	uid, _ := strconv.ParseUint(msg.Uid, 10, 64)
+
+	tipAmount, _ := sdk.NewIntFromString(msg.Amount)
+
+	tip := sdk.NewCoin("kudos", tipAmount)
+
+	// Test if post exists
+	post, found := k.GetPost(ctx, uid)
+	if !found {
+		return nil, sdkerrors.Wrapf(types.ErrPostDoesNotExist, "%s", msg.Uid)
+	}
+
+	post.Tips = post.Tips.Add(tip)
+
+	k.SetPost(ctx, post)
+
+	haven, _ := k.GetHaven(ctx, post.Haven)
+
+	havenTip := (tip.Amount.Mul(sdk.NewIntFromUint64(haven.Rake))).Quo(sdk.NewInt(100))
+	userTip := tip.Amount.Sub(havenTip)
+
+	haven.Earnings = haven.Earnings.AddAmount(havenTip)
 
 	return &types.MsgTipPostResponse{}, nil
 }
--- x/haven/types/errors.go
+++ x/haven/types/errors.go
@@ -12,4 +12,6 @@ var (
 	ErrHavenAlreadyExists = sdkerrors.Register(ModuleName, 1, "the haven already exists") // nolint: gomnd
 	// ErrHavenAlreadyExists - haven with proposed name already exists
 	ErrHavenDoesNotExist = sdkerrors.Register(ModuleName, 2, "the haven does not exist") // nolint: gomnd
+	// ErrPostDoesNotExist - post does not exist
+	ErrPostDoesNotExist = sdkerrors.Register(ModuleName, 3, "the post does not exist") // nolint: gomnd
 )
