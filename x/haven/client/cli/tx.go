39bb6b01046d2882c3086db25363c5a0a91c063e
Aaron Kutch <aaronkutch@att.net>
2023-07-12 18:31:57 -0500

    cleanup

--- x/haven/client/cli/tx.go
+++ x/haven/client/cli/tx.go
@@ -7,16 +7,10 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/cosmos/cosmos-sdk/client"
-	// "github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/onomyprotocol/haven/x/haven/types"
 )
 
 var DefaultRelativePacketTimeoutTimestamp = uint64((time.Duration(10) * time.Minute).Nanoseconds())
-
-const (
-	flagPacketTimeoutTimestamp = "packet-timeout-timestamp"
-	listSeparator              = ","
-)
 
 // GetTxCmd returns the transaction commands for this module
 func GetTxCmd() *cobra.Command {
